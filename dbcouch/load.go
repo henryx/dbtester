3bfdcbf54cd310db45f9dbf956fe4bb2465bdd02
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-09-14 09:49:15 +0200

    Implemented JSON load

--- dbcouch/load.go
+++ dbcouch/load.go
@@ -2,17 +2,36 @@ package dbcouch
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
-func (e *CouchDB) save(buf string) {
-	// TODO: implement save
+func (c *CouchDB) save(buf string) {
+	resp := c.call("POST", fmt.Sprintf("http://%s:%s@%s:%d/%s/_bulk_docs", c.user, c.password, c.host, c.port, c.database), bytes.NewBuffer([]byte(buf)))
+
+	body, err := ioutil.ReadAll(resp.Body)
+	msg := string(body)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		if err != nil {
+			panic(err)
+		} else {
+			log.Println(resp.Status)
+			panic(msg)
+		}
+	}
 }
 
 func (c *CouchDB) Load(size int, filename string) {
 	var err error
+	var buf strings.Builder
+	var data strings.Builder
 
 	inFile, err := os.Open(filename)
 	if err != nil {
@@ -24,13 +43,21 @@ func (c *CouchDB) Load(size int, filename string) {
 
 	counter := 0
 	commit := 0
-	buf := ""
+
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
-				if buf != "" {
-					c.save(buf)
+				if buf.Len() > 0 {
+					data.Reset()
+
+					buffer := buf.String()
+					buffer = strings.TrimSuffix(buffer, ",")
+
+					data.WriteString(`{"docs": [`)
+					data.WriteString(buffer)
+					data.WriteString(`]}`)
+					c.save(data.String())
 				}
 				break
 			}
@@ -43,12 +70,28 @@ func (c *CouchDB) Load(size int, filename string) {
 		}
 		counter++
 
+		line = strings.TrimRightFunc(line, func(c rune) bool {
+			//In windows newline is \r\n
+			return c == '\r' || c == '\n'
+		})
+		buf.WriteString(line)
+		buf.WriteString(",")
+
 		if counter == size {
-			c.save(buf)
+			data.Reset()
+
+			buffer := buf.String()
+			buffer = strings.TrimSuffix(buffer, ",")
+
+			data.WriteString(`{"docs": [`)
+			data.WriteString(buffer)
+			data.WriteString(`]}`)
+			
+			c.save(data.String())
 			commit++
 			log.Printf("Committed %d...\n", commit)
 
-			buf = ""
+			buf.Reset()
 			counter = 0
 		}
 	}
