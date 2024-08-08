1cebafee2373ed7906cf2b1946676094d3536d6c
Enrico Bianchi <enrico.bianchi@gmail.com>
2024-08-08 18:18:29 +0200

    Implemented code to add a relation from edition and publishers

--- dbsqlite/schema.go
+++ dbsqlite/schema.go
@@ -3,6 +3,7 @@ package dbsqlite
 import (
 	"database/sql"
 	"dbtest/common"
+	"errors"
 	"github.com/tidwall/gjson"
 )
 
@@ -46,7 +47,7 @@ func (db *SQLite) addEdition(tx *sql.Tx, j string) {
 	format := gjson.Get(j, "format")
 	publishDate := gjson.Get(j, "publish_date")
 	edition := gjson.Get(j, "edition_name")
-
+	publishers := gjson.Get(j, "publishers")
 	desc := gjson.Get(j, "description")
 
 	description := ""
@@ -68,6 +69,27 @@ func (db *SQLite) addEdition(tx *sql.Tx, j string) {
 	err = stmt.QueryRow(key.String(), title.String(), nullString(subtitle), nullString(format),
 		nullString(publishDate), nullString(edition), description, nullString(pages)).Scan(&editionId)
 
+	publishersIds := db.getPublishers(tx, publishers.Array())
+	db.addEditionPublishers(tx, editionId, publishersIds)
+}
+
+func (db *SQLite) addEditionPublishers(tx *sql.Tx, editionId int, publishers []int) {
+	var err error
+
+	insert := "INSERT INTO editions_publishers(edition_id, publisher_id) VALUES(?, ?)"
+
+	stmt, err := tx.Prepare(insert)
+	if err != nil {
+		panic("Cannot create statement: " + err.Error())
+	}
+	defer stmt.Close()
+
+	for _, publisher := range publishers {
+		_, err := stmt.Exec(editionId, publisher)
+		if err != nil {
+			panic("Cannot insert publishers/editions relation: " + err.Error())
+		}
+	}
 }
 
 func (db *SQLite) addAuthor(tx *sql.Tx, j string) {
@@ -92,6 +114,38 @@ func (db *SQLite) addAuthor(tx *sql.Tx, j string) {
 	if err != nil {
 		panic("Cannot execute query: " + err.Error())
 	}
+}
+
+func (db *SQLite) getPublishers(tx *sql.Tx, publishers []gjson.Result) []int {
+	var publisherId int
+
+	res := make([]int, 0)
+	query := "SELECT id FROM publishers WHERE name = ?"
+
+	for _, publisher := range publishers {
+		err := tx.QueryRow(query, publisher.String()).Scan(&publisherId)
+		if !errors.Is(err, sql.ErrNoRows) {
+			if err != nil {
+				panic("Cannot get publishers: " + err.Error())
+			}
+		}
+
+		if publisherId == 0 {
+			insert := "INSERT INTO publishers(name) VALUES (?) RETURNING id"
+			stmt, err := tx.Prepare(insert)
+			if err != nil {
+				return nil
+			}
+			defer stmt.Close()
+
+			err = stmt.QueryRow(publisher.String()).Scan(&publisherId)
+			if err != nil {
+				return nil
+			}
+		}
+		res = append(res, publisherId)
+	}
+	return res
 }
 
 func (db *SQLite) loadAuthors(tx *sql.Tx) {
