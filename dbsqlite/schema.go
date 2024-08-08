197863ac90dc47a0d590549e946dc9d43fd58d09
Enrico Bianchi <enrico.bianchi@gmail.com>
2024-08-08 18:17:40 +0200

    Implemented code to add an edition

--- dbsqlite/schema.go
+++ dbsqlite/schema.go
@@ -16,6 +16,60 @@ func nullInt(v gjson.Result) sql.NullInt64 {
 	}
 }
 
+func nullString(v gjson.Result) sql.NullString {
+	if v.Type == gjson.Null {
+		return sql.NullString{}
+	}
+
+	return sql.NullString{
+		String: v.String(),
+		Valid:  true,
+	}
+}
+
+func (db *SQLite) addEdition(tx *sql.Tx, j string) {
+	var editionId int
+	var err error
+	var insert = "INSERT INTO editions(key, title, subtitle, format, publish_date, edition, description, pages) VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id"
+
+	key := gjson.Get(j, "key")
+	if !key.Exists() {
+		return
+	}
+
+	title := gjson.Get(j, "title")
+	if !title.Exists() {
+		return
+	}
+
+	subtitle := gjson.Get(j, "subtitle")
+	format := gjson.Get(j, "format")
+	publishDate := gjson.Get(j, "publish_date")
+	edition := gjson.Get(j, "edition_name")
+
+	desc := gjson.Get(j, "description")
+
+	description := ""
+	switch desc.Type {
+	case gjson.String:
+		description = desc.String()
+	case gjson.JSON:
+		description = desc.Get("description.value").String()
+	}
+
+	pages := gjson.Get(j, "number_of_pages")
+
+	stmt, err := tx.Prepare(insert)
+	if err != nil {
+		panic("Cannot create statement: " + err.Error())
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(key.String(), title.String(), nullString(subtitle), nullString(format),
+		nullString(publishDate), nullString(edition), description, nullString(pages)).Scan(&editionId)
+
+}
+
 func (db *SQLite) addAuthor(tx *sql.Tx, j string) {
 	var err error
 	var insert = "INSERT INTO authors(key, name, revision, birth, death) VALUES(?, ?, ?, ?, ?)"
@@ -60,13 +114,58 @@ func (db *SQLite) loadAuthors(tx *sql.Tx) {
 	}
 }
 
+func (db *SQLite) loadEditions(tx *sql.Tx) {
+	var err error
+
+	query := `SELECT data FROM json_data WHERE data->'type'->>'key' = ?`
+	j := ""
+
+	tx, err = db.conn.Begin()
+	if err != nil {
+		panic("Cannot open transaction: " + err.Error())
+	}
+
+	row, err := tx.Query(query, common.EDITIONS)
+	if err != nil {
+		panic("Cannot execute query: " + err.Error())
+	}
+
+	for row.Next() {
+		err = row.Scan(&j)
+		if err != nil {
+			panic("Cannot read value: " + err.Error())
+		}
+		db.addEdition(tx, j)
+
+		err = tx.Commit()
+		if err != nil {
+			panic("Cannot commit transaction: " + err.Error())
+		}
+	}
+}
+
 func (db *SQLite) loadSchema() {
-	tx, err := db.conn.Begin()
+	var tx *sql.Tx
+	var err error
+
+	tx, err = db.conn.Begin()
 	if err != nil {
 		panic("Cannot open transaction: " + err.Error())
 	}
 
 	db.loadAuthors(tx)
+
+	err = tx.Commit()
+	if err != nil {
+		panic("Cannot commit transaction: " + err.Error())
+	}
+
+	tx, err = db.conn.Begin()
+	if err != nil {
+		panic("Cannot open transaction: " + err.Error())
+	}
+
+	db.loadEditions(tx)
 
 	err = tx.Commit()
 	if err != nil {
